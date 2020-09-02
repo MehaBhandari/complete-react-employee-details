f764bcc2b6c6e59dae1405868e9ad0655bf7856a
Meha <ahem4progs@gmail.com>
2020-09-02 12:50:17 +0530

    delete functionality implemented

---
+++ src/component/AddEmployeeWorkingWithClass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class AddEmployee extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
--- src/component/RenderEmpArrayWorkingWithProps.js
+++ src/component/RenderEmpArrayWorkingWithProps.js
@@ -1,18 +1,17 @@
 import React from 'react'
 
-
 export default function WorkingWithProps(props) {
     return(
         <div className="card-group container-fluid" style={{'marginLeft': '15px'}}>
             <div className="row">
-                {props.employeeArray.map((empObj) => {                
+                {props.employeeArray.map((empObj) => { 
                     return(                    
                         <div className="card col-3" key={empObj.empId} style={{'width': '18rem'}}>
-                            <img className="card-img-top" src={empObj.imgUrl} alt="Card image cap" />
+                            <img className="card-img-top" src={empObj.imgUrl} alt="Card image cap" style={{'minHeight': '300px','maxHeight': '300px', 'maxWidth': '250px'}} />
                             <div className="card-body">
                                 <h5 className="card-title">{empObj.name}</h5>
                                 <p className="card-text">{empObj.designation}</p>
-                                <a href="#" className="btn btn-primary">Delete Employee</a>
+                                <input type='button' className="btn btn-primary" onClick={() => props.deleteFunction(empObj.empId)} value='Delete Employee' />
                             </div>
                         </div>
                     )
